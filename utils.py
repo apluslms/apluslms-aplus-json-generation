e726ff57c6d0b6e4288417d060b7c33e8ea49d1c
QianqianQ <qianqian.qin@outlook.com>
2019-08-27 14:43:36 +0300

    test the request to mooc-grader

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ import tarfile
 import requests
 from math import floor
 from operator import itemgetter
+import json, yaml
 
 
 def url_to_static(static_url, course_key, path):
@@ -38,6 +39,60 @@ def update_index_yaml(static_url, course_key, course_data):
     if "modules" in course_data:
         for m in course_data["modules"]:
             children_recursion(m)
+
+
+class ParseError(Exception):
+    def __init__(self, value, error=None):
+        self.value = value
+        self.error = error
+
+    def __str__(self):
+        if self.error is not None:
+            return "%s: %s" % (repr(self.value), repr(self.error))
+        return repr(self.value)
+
+
+class Parser:
+
+    FORMATS = {
+        'json': json.load,
+        'yaml': yaml.safe_load,
+        'yml': yaml.safe_load
+    }
+
+    def __init__(self, course_dir):
+        '''
+        The constructor.
+        '''
+        self._DIR = course_dir
+
+    def parse(self, path, loader=None):
+        '''
+        Parses a dict from a file.
+        @type path: C{str}
+        @param path: a path to a file
+        @type loader: C{function}
+        @param loader: a configuration file stream parser
+        @rtype: C{dict}
+        @return: an object representing the configuration file or None
+        '''
+        if not loader:
+            try:
+                loader = self.FORMATS[os.path.splitext(path)[1][1:]]
+            except:
+                raise ParseError('Unsupported format "%s"' % path)
+        data = None
+        path = os.path.join(self._DIR, path)
+        try:
+            with open(path) as f:
+                try:
+                    data = loader(f)
+                except ValueError as e:
+                    raise ParseError("Configuration error in %s" % path, e)
+        except OSError as e:
+            print(e)
+
+        return data
 
 
 def error_print():
--- validate.py
+++ validate.py
@@ -1,10 +1,8 @@
 import logging
 from apluslms_yamlidator.validator import Validator
 from apluslms_yamlidator import schemas
-from apluslms_yamlidator.utils.yaml import rt_dump
-import json
+# from apluslms_yamlidator.utils.yaml import rt_dump
 from utils import *
-
 
 logger = logging.getLogger(__name__)
 
@@ -25,12 +23,13 @@ with open(generated_dir+"schema.json", "w") as schema_json:
 
 
 # Load and parse course index.yaml
-_, data_loader = schemas.get_file_loader('index_instance/index.yaml')
-course_data = data_loader()
-# course = rt_load(open('index_instance/index.yaml','rb').read())
+# _, data_loader = schemas.get_file_loader('index_instance/index.yaml')
+# course_data = data_loader()
+# # course = rt_load(open('index_instance/index.yaml','rb').read())
+parser = Parser(os.getcwd())
+course_data = parser.parse('index_instance/index.yaml')
 with open(generated_dir+"course.json", "w") as course_json:
     json.dump(course_data, course_json, indent=4, sort_keys=True)
-
 
 # Validate index.yaml
 # if not validator.validate(course_data, 'index', major=1):
@@ -41,15 +40,29 @@ update_index_yaml(STATIC_URL, course_key, course_data)
 
 # Save the updated index.yaml
 with open(generated_dir+'update.yaml', 'w', encoding='utf8') as update_yaml:
-    rt_dump(course_data, update_yaml, allow_unicode=True)
-_, data_loader = schemas.get_file_loader('update.yaml')
+    yaml.dump(course_data, update_yaml,default_flow_style=False)
+_, data_loader = schemas.get_file_loader(generated_dir+'update.yaml')
 update = data_loader()
 with open(generated_dir+"update.json", "w") as update_json:
     json.dump(update, update_json, indent=4, sort_keys=True)
 
 
-# # Send to mooc-grader
-# request_url = 'http://0.0.0.0:8080/api/v1/validate/'
-# request = requests.post(request_url, data={'status': 'success'})
+os.environ['PLUGIN_API'] = 'http://0.0.0.0:8080/api/v1/'
+os.environ['PLUGIN_TOKEN'] = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJkZWZfY291cnNlIiwiaWF0IjoxNTYyODI4MzA0L' \
+                             'CJpc3MiOiJzaGVwaGVyZCJ9.MUkoD27P6qZKKMM5juL0e0pZl8OVH6S17N_ZFzC7D0cwOgbcDaAO3S1BauXzhQ' \
+                             'OneChPs1KEzUxI2dVF-Od_gpN8_IJEnQnk25XmZYecfdoJ5ST-6YonVmUMzKP7UAcvzCFye7mkX7zJ1ADYtda5' \
+                             '7IUdyaLSPOWnFBSHX5B4XTzzPdVZu1xkRtb17nhA20SUg9gwCOPD6uLU4ml1aOPHBdiMLKz66inI8txPrRK57G' \
+                             'n33m8lVp0WTOOgLV5MkCIpkgVHBl50EHcQFA5KfPet3FBLjpp2I1yThQe_n1Zc6GdnR0v_nqX0JhmmDMOvJ5rh' \
+                             'IHZ7B0hEtFy9rKUWOWfcug'
+os.environ['PLUGIN_COURSE'] = "def_course"
 
+update_url = os.environ['PLUGIN_API'] + os.environ['PLUGIN_COURSE'] + '/update'
+
+headers = {
+        'Authorization': 'Bearer {}'.format(os.environ['PLUGIN_TOKEN'])
+        }
+
+# # Send a request to mooc-grader to update index.yaml
+request = requests.post(update_url, headers=headers, data={'status': 'success'})
+print(request.text)
 
